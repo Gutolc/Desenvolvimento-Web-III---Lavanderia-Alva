48f176dcf15073b79dcb4f597a5782dd178c16f0
Gustavo Lins Cintra <gustavo.cintra@fatec.sp.gov.br>
2026-09-15 22:24:03 -0300

    Add files via upload

---
+++ Backend/Backend/alva-backend/src/main/java/com/walkers/alva/AlvaApplication.java
@@ -0,0 +1,13 @@
+package com.walkers.alva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlvaApplication.class, args);
+	}
+
+}
---
+++ Backend/Backend/alva-backend/src/main/java/com/walkers/alva/dto/response/AuthResponseDTO.java
@@ -0,0 +1,4 @@
+package com.walkers.alva.dto.response;
+
+public record AuthResponseDTO(String token, String perfil) {
+}
